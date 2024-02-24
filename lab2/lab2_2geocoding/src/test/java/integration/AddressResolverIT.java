8714928dc58f9c890c8070abe4b11714ed6b223e
Miguel da Silva Pinto <miguel.silva48@ua.pt>
2024-02-24 16:18:28 +0000

    Feat(lab2): Exercise 2_3 completed

--- lab2/lab2_2geocoding/src/test/java/integration/AddressResolverIT.java
+++ lab2/lab2_2geocoding/src/test/java/integration/AddressResolverIT.java
@@ -2,40 +2,48 @@ package integration;
 
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import connection.TqsBasicHttpClient;
+import geocoding.Address;
+import geocoding.AddressResolverService;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AddressResolverIT {
 
+    private AddressResolverService resolver;
 
     @BeforeEach
     public void init(){
+        resolver = new AddressResolverService(new TqsBasicHttpClient());
     }
 
-
-    @Disabled
     @Test
     public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
 
-        //todo
-
         // repeat the same tests conditions from AddressResolverTest, without mocks
+        Optional<Address> result = resolver.findAddressForLocation(40.63436, -8.65616);
 
+        Address expected = new Address( "Avenida da Universidade", "Aveiro","3810-489", "");
+
+        assertTrue( result.isPresent());
+        assertEquals( expected, result.get());
     }
 
-    @Disabled
     @Test
     public void whenBadCoordidates_thenReturnNoValidAddrress() throws IOException, URISyntaxException, ParseException {
 
-        //todo
         // repeat the same tests conditions from AddressResolverTest, without mocks
+        Optional<Address> result = resolver.findAddressForLocation( -361,-361);
         
+        assertFalse( result.isPresent());
     }
 
 }
